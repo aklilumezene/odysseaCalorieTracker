d4f23b347b480ba5c40d3fbef9609f4721a10ddb
aklilu <aklilumezene@gmail.com>
2021-06-08 01:14:31 +0300

    Add MealItem: Added mockup data

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,5 +1,13 @@
 const MealItem = () => {
-    return <p>This is a meal</p>
+    return <div>
+        <div>07/6 08:29</div>
+        <div>Lunch</div>
+        <div>cookie</div>
+        <div>100</div>
+        <div>1</div>
+        <div>534</div>
+        <div>1</div>
+    </div>
 }
 
 export default MealItem
