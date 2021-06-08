55d8592bc78ddb443e7dedab72dfc65583801987
aklilu <aklilumezene@gmail.com>
2021-06-08 04:34:09 +0300

    MetricItem ~ values as variables

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,17 +1,28 @@
 import "./MealItem.css";
 
 const MealItem = () => {
-  return (
+  let mealDate = new Date(2021, 5, 8, 4, 7, 0, 0);
+  let regularMeal = "Lunch";
+  let foodServed = "Cookie";
+  let calories = 100;
+  let fat = 1;
+  let protein = 534;
+  let carbs = 1;
+
+
+
+  
+    return (
     <div className="meal-item">
-      <div className="meal-item_date">07/6 08:29</div>
+      <div className="meal-item_date">{mealDate}</div>
       <div className="meal-item_meal">
-        <div>Lunch</div>
-        <h3>Cookie</h3>
+        <div>{regularMeal}</div>
+        <h3>{foodServed}</h3>
       </div>
-      <div className="meal-item_calories">100</div>
-      <div className="meal-item_fat">1</div>
-      <div className="meal-item_protein">534</div>
-      <div className="meal-item_carbs">1</div>
+      <div className="meal-item_calories">{calories}</div>
+      <div className="meal-item_fat">{fat}</div>
+      <div className="meal-item_protein">{protein}</div>
+      <div className="meal-item_carbs">{carbs}</div>
     </div>
   );
 };
