e19cd09ca7802ba7be4553380e8aad9c6313c29a
aklilu <aklilumezene@gmail.com>
2021-06-08 04:00:04 +0300

    Add MealItem: use css

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,13 +1,19 @@
-const MealItem = () => {
-    return <div>
-        <div>07/6 08:29</div>
-        <div>Lunch</div>
-        <div>cookie</div>
-        <div>100</div>
-        <div>1</div>
-        <div>534</div>
-        <div>1</div>
-    </div>
-}
+import "./MealItem.css";
 
-export default MealItem
+const MealItem = () => {
+  return (
+    <div className="meal-item">
+      <div className="meal-item_date">07/6 08:29</div>
+      <div className="meal-item_meal">
+        <div>Lunch</div>
+        <h3>Cookie</h3>
+      </div>
+      <div className="meal-item_calories">100</div>
+      <div className="meal-item_fat">1</div>
+      <div className="meal-item_protein">534</div>
+      <div className="meal-item_carbs">1</div>
+    </div>
+  );
+};
+
+export default MealItem;
