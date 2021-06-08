8501f41ea311ed68c044713903fa33588b1f9a95
aklilu <aklilumezene@gmail.com>
2021-06-08 23:53:12 +0300

    MealItem ~ values as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import MealItem from "./components/MealItem"
 
 function App() {
+  let mealDate = new Date(2021, 5, 8, 4, 7, 0, 0);
+  let regularMeal = "Lunch";
+  let foodServed = "Cookie";
+  let calories = 100;
+  let fat = 1;
+  let protein = 534;
+  let carbs = 1;
+  
   return (
     <div className="App">
     <h1>Hello React</h1>
-    <MealItem />
+    <MealItem 
+    date = {mealDate}
+    regularMeal = {regularMeal}
+    foodServed = {foodServed}
+    calories = {calories}
+    fat = {fat}
+    protein = {protein}
+    carbs = {carbs}
+
+    />
     </div>
   
   );
--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,28 +1,19 @@
 import "./MealItem.css";
 
-const MealItem = () => {
-  let mealDate = new Date(2021, 5, 8, 4, 7, 0, 0);
-  let regularMeal = "Lunch";
-  let foodServed = "Cookie";
-  let calories = 100;
-  let fat = 1;
-  let protein = 534;
-  let carbs = 1;
-
-
-
+const MealItem = (props) => {
   
-    return (
+
+  return (
     <div className="meal-item">
-      <div className="meal-item_date">{mealDate}</div>
+      <div className="meal-item_date">{props.date.toISOString()}</div>
       <div className="meal-item_meal">
-        <div>{regularMeal}</div>
-        <h3>{foodServed}</h3>
+        <h3>{props.regularMeal}</h3>
+        <div>{props.foodServed}</div>
       </div>
-      <div className="meal-item_calories">{calories}</div>
-      <div className="meal-item_fat">{fat}</div>
-      <div className="meal-item_protein">{protein}</div>
-      <div className="meal-item_carbs">{carbs}</div>
+      <div className="meal-item_calories">{props.calories}</div>
+      <div className="meal-item_fat">{props.fat}</div>
+      <div className="meal-item_protein">{props.protein}</div>
+      <div className="meal-item_carbs">{props.carbs}</div>
     </div>
   );
 };
